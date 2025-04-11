e26d7bf34100babafd4da77d40a7003ad26684a8
Dhiraj <129248943+Dhiraj10002@users.noreply.github.com>
2025-04-11 16:32:01 +0530

    Add files via upload

---
+++ 02_Bmi_calculator/project2.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit' , function (e) {
+     e.preventDefault();
+
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const results = this.querySelector('#results');
+const weight_guide = this.querySelector('#weight_guide')
+
+if(height === '' || height <0 || isNaN(height)){
+    results.innerHTML = `Please give a valid height ${height}`;
+}
+else if(weight === '' || weight <0 || isNaN(weight)){
+    results.innerHTML = ` Please give a valid wight ${weight}`;
+}
+else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+   // show the results
+   results.innerHTML = `<span>${bmi}</span>`;
+}
+
+});
+
+
+// try to create weight guide but not succeed may be i try later or someday
